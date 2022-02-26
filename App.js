76071c34063b97cf88cc827c8ad3cd39fbb8c084
sim1029 <sps67@pitt.edu>
2022-02-25 20:14:10 -0500

    Create inital welcome/loading screen

--- App.js
+++ App.js
@@ -1,10 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.grocyText}>Easy Grocy</Text>
+      <Image 
+      source={require('./assets/icon/groceries.png')}
+      style={styles.grocyLogo}
+      ></Image>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +17,19 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#7FC8A9',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  grocyLogo: {
+    width: 125,
+    height: 125,
+    resizeMode: "contain",
+    margin: 50,
+  },
+  grocyText: {
+    color: "floralwhite",
+    fontSize: 50,
+    textAlign: "center",
   },
 });
