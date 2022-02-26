85b5a140b255974d29605e3d88786bf9f6dc3a3e
sim1029 <sps67@pitt.edu>
2022-02-26 00:30:10 -0500

    Finish welcome/loading screen and create navigation Tab bar and shells for screen Home, Shopping List, Profile

--- App.js
+++ App.js
@@ -1,35 +1,7 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import GrocyStack from './components/GrocyStack'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.grocyText}>Easy Grocy</Text>
-      <Image 
-      source={require('./assets/icon/groceries.png')}
-      style={styles.grocyLogo}
-      ></Image>
-      <StatusBar style="auto" />
-    </View>
+    <GrocyStack></GrocyStack>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#7FC8A9',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  grocyLogo: {
-    width: 125,
-    height: 125,
-    resizeMode: "contain",
-    margin: 50,
-  },
-  grocyText: {
-    color: "floralwhite",
-    fontSize: 50,
-    textAlign: "center",
-  },
-});
