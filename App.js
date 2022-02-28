2a6c089d7c83966e7d441bbb985ab52de69142e1
sim1029 <sps67@pitt.edu>
2022-02-28 13:35:32 -0500

    get main up to date

--- App.js
+++ App.js
@@ -1,18 +1,7 @@
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> c34154390619e8ae0d7233b4b891e69cd0721ab6
 import React, { useEffect, useState, useCallback } from "react";
 import { View, Text } from "react-native";
 import * as SplashScreen from 'expo-splash-screen';
 import Auth from './components/Auth';
-<<<<<<< HEAD
-=======
-=======
-import GrocyStack from './components/GrocyStack'
-import Auth from './components/Auth'
->>>>>>> a546eee61e56ade61f5ad8f0ecbdc2ac9f9c4ca9
->>>>>>> c34154390619e8ae0d7233b4b891e69cd0721ab6
 
 export default function App() {
   const [appIsReady, setAppIsReady] = useState(false);
@@ -44,14 +33,6 @@ export default function App() {
   }
 
   return (
-<<<<<<< HEAD
       <Auth onLayout={onLayoutRootView}></Auth>
-=======
-<<<<<<< HEAD
-      <Auth onLayout={onLayoutRootView}></Auth>
-=======
-    <Auth></Auth>
->>>>>>> a546eee61e56ade61f5ad8f0ecbdc2ac9f9c4ca9
->>>>>>> c34154390619e8ae0d7233b4b891e69cd0721ab6
   );
 }
